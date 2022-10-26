08240de162f353d8c2c2ffa9e43ab319d6c3f7c6
Brad Hansen <bhansen3@uw.edu>
2022-10-26 00:04:46 -0700

    add

---
+++ download_batch.sh
@@ -0,0 +1,2 @@
+wget -r -c ftp://X202SC22092129-Z01-F001:y4m5p8wu@usftp21.novogene.com:21/
+      
---
+++ download_genome.sh
@@ -0,0 +1,12 @@
+
+# .chr.tf.gz from https://ftp.ensembl.org/pub/release-108/gtf/rattus_norvegicus/
+
+wget https://ftp.ensembl.org/pub/release-108/gtf/rattus_norvegicus/Rattus_norvegicus.mRatBN7.2.108.chr.gtf.gz
+
+
+rsync -av rsync://ftp.ensembl.org/ensembl/pub/release-108/fasta/rattus_norvegicus/dna/ ./
+
+   
+
+ 
+    
