bcc9021f51144bb713ddc5d9b3bf8c5de5339e0d
Brad Hansen <72404908+bchansen3@users.noreply.github.com>
2022-10-26 22:00:27 -0700

    Update mapping_star.sh

--- mapping_star.sh
+++ mapping_star.sh
@@ -14,13 +14,21 @@ index=/bigdata/faustmanlab/bch/rat_150read_genome
 # provide direction to fastq files
 FILES=/bigdata/faustmanlab/bch/gse85420/fastq/*.fq.gz
 
+
+for i in $(raw_data/270,raw_data/272, raw_data/274,raw_data/278C,raw_data/284C); do 
+/DataAnalysis/STAR-2.7.5a/bin/Linux_x86_64/./STAR --genomeDir /DataAnalysis/Manoj-data/test-star/SAindex \
+    --readFilesIn raw_data/${i}_R1.fastq raw_data/${i}_R2.fastq \
+    --runThreadN 8 --outFileNamePrefix aligned/$i. \
+    --outSAMtype BAM SortedByCoordinate \
+    --quantMode GeneCounts; done
+
 for f in $FILES
 do
     echo $f
     base=$(basename $f .fq.gz)
     echo $base
     STAR --runThreadN 4 --genomeDir $index --readFilesIn $f --outSAMtype BAM SortedByCoordinate \
-         --quantMode GeneCounts --readFilesCommand zcat --outFileNamePrefix /bigdata/faustmanlab/bch/gse85420/$base"_"
+         --readFilesCommand zcat --outFileNamePrefix /bigdata/faustmanlab/bch/gse85420/$base"_"
 done
 
 echo "hopefully that worked!"
